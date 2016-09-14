4d64d298ac97d1e6374a37ea2a356293fd542faf
moudroff <moudroff>
2016-09-14 15:52:06 +0300

    Поля, конструкторы, свойства

---
+++ ConsoleApplication6/ConsoleApplication6/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication6
+{
+    class Point
+    {
+        private double x;
+        private double y;
+        
+        public Point():this(1)
+        {
+            //x = y = 1;
+        }
+
+        public Point(double p_x):this(p_x,p_x)
+        {
+           // x = y = p_x;
+        }
+
+        public Point(double p_x, double p_y)
+        {
+            x = p_x;
+            y = p_y;
+        }
+
+
+
+
+        public double M_x
+        {
+            get
+            {
+                return x;
+            }
+            set
+            {
+                x = value;
+             }
+        }
+
+        public double M_y
+        {
+            get
+            {
+                return y;
+            }
+            set
+            {
+                y = value;
+            }
+        }
+
+        //public void SetX(double p_x)
+        //{
+        //    x = p_x;
+        //}
+
+        //public void SetY(double p_y)
+        //{
+        //    y = p_y;
+        //}
+
+        //public double GetX()
+        //{
+        //    return x;
+        //}
+
+        //public double GetY()
+        //{
+        //    return y;
+        //}
+
+        //public void Show()
+        //{
+        //    Console.WriteLine("точка - ({0},{1})", x, y);
+        //}
+
+        public override string ToString()
+        {
+            return String.Format("точка - ({0},{1})", x, y);
+        }
+
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point a = new Point(5);
+            Point b = new Point(2,3);
+            Point c = new Point();
+
+            //a.M_x = -5;
+            //a.M_y = 6;
+            //a.SetX(5);
+            //a.SetY(6);
+            //a.Show();
+
+            Console.WriteLine(c);
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            
+
+        }
+    }
+}
